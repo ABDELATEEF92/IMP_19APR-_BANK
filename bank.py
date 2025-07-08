a0efb016dce04b00040b2c52354b0fa6ec4192ec
ABDELATEEF92 <seemyou.7@gmail.com>
2025-07-08 23:10:14 +0300

    Create bank.py

---
+++ bank.py
@@ -0,0 +1,14 @@
+from User import user
+class Bank(User):
+    
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"The new balance is: {self.balance}")
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            print(f"The new balance is: {self.balance:}")
+        else:
+            print(" The balance was insufficient")
+    def view_balance(self):
+        print(f"The current balance is: {self.balance:}")
