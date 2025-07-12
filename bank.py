3737b433d3cae62c6c6cd6a47a5137ce5d8faf73
ABDELATEEF92 <seemyou.7@gmail.com>
2025-07-12 17:18:25 +0300

    Add files via upload

--- bank.py
+++ bank.py
@@ -1,14 +1,17 @@
-from User import user
+from user import User
+
 class Bank(User):
-    #ABDELATEEF
+    def __init__(self, name , age , gender , balance):
+        super().__init__(name, age, gender, balance)
+
     def deposit(self, amount):
         self.balance += amount
         print(f"The new balance is: {self.balance}")
     def withdraw(self, amount):
         if amount <= self.balance:
             self.balance -= amount
-            print(f"The new balance is: {self.balance:}")
+            print(f"The new balance is: {self.balance}")
         else:
             print(" The balance was insufficient")
     def view_balance(self):
-        print(f"The current balance is: {self.balance:}")
+        print(f"The current balance is: {self.balance}")
